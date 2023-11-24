ca3a19b29126da0c9ad222ba97e2fe3de740da27
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2023-11-24 11:11:35 +0300

    jdhfks

--- src/components/NavTech/NavTech.jsx
+++ src/components/NavTech/NavTech.jsx
@@ -26,7 +26,7 @@ import {
   NotUl,
   NotWrap,
 } from "./NavTech.elements";
-import Logo from "../../assets/MedLogo.png";
+import Logo from "../../assets/MedLogoo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
